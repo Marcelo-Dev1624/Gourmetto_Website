b44ca61730fab9f961d897dac43362b49754f4c6
MarceloVA <100235694+Marcelo-Dev1624@users.noreply.github.com>
2024-05-02 15:29:37 -0600

    BIG UPDATE
    Kept updating products cards
    
    added corresponding styles and the js for it regarding card elements

--- js/products.js
+++ js/products.js
@@ -1,17 +1,19 @@
 
 //Format [Photo,Name,Description,Weight,Price]
 const dataProductos = [
-    ['images/products/pesto.jpg','Pesto Tradicional', 'Delicioso pesto en su receta tradicional', '220g'],
-    ['images/products/cebollas_encurtidas.jpg','Cebollas Encurtidas', 'Descripcion', '220g'],
-    ['images/products/pesto_tomate.jpg','Pesto de Tomates', 'Descripcion', '220g'],
-    ['images/products/higos.jpg','Mermelada de Higos', 'Descripcion', '220g'],
+    ['images/products/pesto.jpg','Pesto Tradicional', 'Delicioso pesto en su receta tradicional', '220g','$5'],
+    ['images/products/cebollas_encurtidas.jpg','Cebollas Encurtidas', 'Descripcion', '220g','$5'],
+    ['images/products/pesto_tomate.jpg','Pesto de Tomates', 'Descripcion', '220g','$5'],
+    ['images/products/higos.jpg','Mermelada de Higos', 'Descripcion', '220g','$5'],
+    
 ]
 
-const products = dataProductos.map(([photo,name, description, weight]) => ({
+const products = dataProductos.map(([photo,name, description, weight,price]) => ({
     photo,
     name,
     description,
     weight,
+    price,
   }));
 
 
@@ -21,11 +23,19 @@ const products = dataProductos.map(([photo,name, description, weight]) => ({
     const productElement = document.createElement('div');
     productElement.classList.add('product');
     productElement.innerHTML = `
-      <img src="${product.photo}" />
-      <div class="product-text-container">
-        <h2>${product.name}</h2>
-        <h3>${product.weight}</h3>
-        <p>${product.description}</p>
+      <div class="product-image-container">
+        <img src="${product.photo}"/>
+      </div>
+      <div class="product-info">
+        <div class="product-text-container">
+          <h2>${product.name}</h2>
+          <h3>${product.weight}</h3>
+          <p>${product.description}</p>
+        </div>
+        <div class="product-pricing-container">
+          <h3>${product.price}</h3>
+          <button><i class="fa-solid fa-cart-shopping"></i>Add to cart</button>
+        </div>
       </div>
     `;
     return productElement;
