ce14c0149f30c55fe7f76310287639ee2c7d4f76
MarceloVA <100235694+Marcelo-Dev1624@users.noreply.github.com>
2024-07-21 16:05:52 -0600

    UPDATES ON PRODUCTS
    Partially fixed cards on mobile, still testing to do, to see if the scalability is properly achieved, more to come

--- js/products.js
+++ js/products.js
@@ -1,10 +1,11 @@
 
 //Format [Photo,Name,Description,Weight,Price]
 const dataProductos = [
-    ['images/products/pesto.jpg','Pesto Tradicional', 'Delicioso pesto en su receta tradicional', '220g','$5'],
+    ['images/bowl.png','Pesto Tradicional', 'Delicioso pesto en su receta tradicional', '220g','$5'],
     ['images/products/cebollas_encurtidas.jpg','Cebollas Encurtidas', 'Descripcion', '220g','$5'],
     ['images/products/pesto_tomate.jpg','Pesto de Tomates', 'Descripcion', '220g','$5'],
     ['images/products/higos.jpg','Mermelada de Higos', 'Descripcion', '220g','$5'],
+   
     
 ]
 
@@ -23,18 +24,18 @@ const products = dataProductos.map(([photo,name, description, weight,price]) => 
     const productElement = document.createElement('div');
     productElement.classList.add('product');
     productElement.innerHTML = `
-      <div class="product-image-container">
+
         <img src="${product.photo}"/>
-      </div>
       <div class="product-info">
         <div class="product-text-container">
-          <h2>${product.name}</h2>
-          <h3>${product.weight}</h3>
+          <h2>${product.name} </br> <span>${product.weight}</span></h2>
           <p>${product.description}</p>
         </div>
         <div class="product-pricing-container">
-          <h3>${product.price}</h3>
-          <button class="btn product-pricing-container-btn"><i class="fa-solid fa-cart-shopping"></i>Add to cart</button>
+          
+            <h3>${product.price}</h3>
+          
+          <button class="btn-size small btn-third-dark-hover mobile-full"><i class="fa-solid fa-cart-shopping"></i> Añadir al carro</button>
         </div>
       </div>
     `;
